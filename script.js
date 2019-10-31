726ed430cc874db55c61ba15e5b1a96c8600a70d
Matthew Kwong <10681480+MatthewKwong@users.noreply.github.com>
2019-10-30 23:49:02 -0700

    improved scroll animation

--- script.js
+++ script.js
@@ -7,19 +7,19 @@ $(document).ready(function () {
   });
 });
 
-$(window).on("load",function() {
-  $(window).scroll(function() {
-    var windowBottom = $(this).scrollTop() + $(this).innerHeight() ;
-    $(".project-img").each(function() {
-      /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
 
-      /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")==0) {$(this).fadeTo(450,1);}
-      } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity")==1) {$(this).fadeTo(450,0);}
+$(document).on("scroll", function () {
+  var pageTop = $(document).scrollTop()
+  var pageBottom = pageTop + $(window).height()
+  var tags = $(".project")
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i]
+    if ($(tag).position().top < pageBottom) {
+    $(tag).addClass("visible")
+    }
+    else {
+      $(tag).removeClass("visible")
       }
-    });
-  }).scroll(); //invoke scroll-handler on page-load
-});
+  }
+})
